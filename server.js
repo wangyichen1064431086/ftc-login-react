fcb717c4e78ffb8fa770f62327f3c3b2981766cf
wangyichen1064431086 <1064431086@qq.com>
2018-05-06 20:34:29 +0800

    Modify the server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const path = require('path');
-const express = require('express');//待修改为koa
+//const express = require('express');//待修改为koa
+const Koa = require('koa');
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
 const webpack = require('webpack');
 const config = require('./webpack.config.dev');
 
-const app = express();
+//const app = express();
+const app = new Koa();
 const compiler = webpack(config);
 
 const webpackDevOptions = {
@@ -15,14 +21,22 @@ const webpackDevOptions = {
   }
 };
 
+app.user(logger());
+
+app.user(bodyParser());
+
 app.use(require('webpack-dev-middleware')(compiler, webpackDevOptions));
 
 app.use(require('webpack-hot-middleware')(compiler));
-
+/*
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'demo', 'views', 'app.html'));
 });
-
+*/
+router.get('/', ctx => {
+  ctx.body = path.join(__dirname, 'demo', 'views', 'app.html');
+});
+/*
 app.listen(9000, '0.0.0.0', err => {
   if (err) {
     console.log(err);
@@ -30,3 +44,6 @@ app.listen(9000, '0.0.0.0', err => {
   }
   console.log('Listening at http://localhost:9000');
 });
+*/
+app.user(router);
+app.listen(9000);
