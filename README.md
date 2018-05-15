77cb4178a34ede8cceb6e796a6ffd96d16d864ce
wangyichen1064431086 <1064431086@qq.com>
2018-05-15 15:27:16 +0800

    Some modify

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 [![](https://travis-ci.org/wangyichen1064431086/ftc-login-react.svg?branch=master)](https://travis-ci.org/wangyichen1064431086/ftc-login-react)
 
 <!-- MARKDOWN 插图基础格式： ![Alt text](图片链接) -->
+
+The login component for FTC. React version. It is a part of ftc-header-react.
+
+## Install
+```
+cd yourProject
+npm install react react-dom prop-types
+npm install "@ftchinese/ftc-login-react" --save 
+```
--- demo/src/app.js
+++ demo/src/app.js
@@ -1,47 +1,8 @@
-//import Login from '../../src/js/Login.js';
-import Login from '../../build/index.es.js';
+import Login from '../../src/js/Login.js';
+//import Login from '../../build/index.es.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*
-import Nav from '@ftchinese/ftc-nav-react';
-const  testData2 = [
-  {
-    "name": "首页",
-    "url": "http://www.ftchinese.com",
-    "order":0,
-    "subs":[
-      {
-        "name":"特别报道",
-        "url":"http://www.ftchinese.com/channel/special.html",
-        "order":0
-      },
-      {
-        "name":"热门文章",
-        "url":"http://www.ftchinese.com/channel/special.html",
-        "order":1
-      }
-    ]  
-  },
-  {
-    "name": "中国",
-    "url": "http://www.ftchinese.com/channel/china.html",
-    "order":1,
-    "subs":[
-      {
-        "name":"政经",
-        "url":"http://www.ftchinese.com/channel/chinareport.html",
-        "order":0
-      },
-      {
-        "name":"商业",
-        "url":"http://www.ftchinese.com/channel/chinabusiness.html",
-        "order":1
-      }
-      
-    ]
-  }
-];
-*/
+
 ReactDOM.render(
   <Login postUrl="/users/login" findPasswordUrl="http://www.ftchinese.com/users/findpassword" registerUrl="http://user.ftchinese.com/register" />,
   //<Nav channels={testData2} dynamicnav={true} />,
